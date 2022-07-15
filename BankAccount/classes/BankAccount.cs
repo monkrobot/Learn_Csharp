053a97854a4509e90ee27ddccd703384909dc114
DenisovAV <DenisovAV@vest-service.ru>
2022-07-15 17:24:22 +0300

    start next lesson

--- BankAccount/classes/BankAccount.cs
+++ BankAccount/classes/BankAccount.cs
@@ -19,16 +19,37 @@ public class BankAccount
         }
     }
 
-    public BankAccount(string name, decimal initialBalance)
+    private readonly decimal _minimumBalance;
+
+    public BankAccount(string name, decimal initialBalance) : this(name, initialBalance, 0) { }
+
+    public BankAccount(string name, decimal initialBalance, decimal minimumBalance)
     {
-        Number = accountNumberSeed.ToString();
-        accountNumberSeed++;
-    
+        Number = s_accountNumberSeed.ToString();
+        s_accountNumberSeed++;
+
         Owner = name;
-        MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+        _minimumBalance = minimumBalance;
+        if (initialBalance > 0)
+            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
     }
 
     private List<Transaction> allTransactions = new List<Transaction>();
+
+    public string GetAccountHistory()
+    {
+        var report = new System.Text.StringBuilder();
+
+        decimal balance = 0;
+        report.AppendLine("Date\t\tAmount\tBalance\tNote");
+        foreach (var item in allTransactions)
+        {
+            balance += item.Amount;
+            report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{balance}\t{item.Notes}");
+        }
+
+        return report.ToString();
+    }
 
     public void MakeDeposit(decimal amount, DateTime date, string note)
     {
@@ -42,7 +63,7 @@ public class BankAccount
 
     public void MakeWithdrawal(decimal amount, DateTime date, string note)
     {
-        if (amount <= 0)
+        if (amount <= minimumBalance)
         {
             throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
         }
@@ -52,5 +73,10 @@ public class BankAccount
         }
         var withdrawal = new Transaction(-amount, date, note);
         allTransactions.Add(withdrawal);
+    }
+
+    public virtual void PerformMonthEndTransactions() 
+    { 
+
     }
 }
